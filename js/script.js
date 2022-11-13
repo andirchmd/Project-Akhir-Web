7390a44db6db3a22b1f66efcb6f6d8c0fb154197
Arifwijaksono <114127157+Arifwijaksono@users.noreply.github.com>
2022-11-13 21:43:01 +0800

    pindah js

---
+++ js/script.js
@@ -0,0 +1,45 @@
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelector('body');
+const box = document.querySelector('section');
+const nav = document.querySelector('header');
+
+toggle.addEventListener('click', function(){
+    this.classList.toggle('bi-moon');
+    if(this.classList.toggle('bi-brightness-high-fill')){
+        alert('Ganti ke Light Mode?');
+        body.style.background = '#ffffff';
+        body.style.color = 'black';
+        box.style.background = 'white';
+        nav.style.background = '#ffffff';
+        nav.style.color = 'black';
+        body.style.transition = '2s';
+        box.style.transition = '2s';
+        nav.style.transition = '2s';
+    }else{
+        alert('Ganti ke Dark Mode?');
+        body.style.background = '#0d0c0c';
+        body.style.color = 'black';
+        box.style.background = '#242323';
+        nav.style.background = '#151515';
+        nav.style.color = 'white';
+        body.style.transition = '2s';
+        box.style.transition = '2s';
+        nav.style.transition = '2s';
+    }
+});
+
+  // const newspaperSpinning = [
+  //   { transform: 'rotate(0) scale(1)' },
+  //   { transform: 'rotate(360deg) scale(0)' }
+  // ];
+  
+  // const newspaperTiming = {
+  //   duration: 2000,
+  //   iterations: 1,
+  // }
+  
+  // const newspaper = document.querySelector(".newspaper");
+  
+  // newspaper.addEventListener('click', () => {
+  //   newspaper.animate(newspaperSpinning, newspaperTiming);
+  // });
